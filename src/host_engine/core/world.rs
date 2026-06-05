9435e45c7a61d230b51ec019060731038187bafe
闷香红烧鱼 <1561182272@qq.com>
2026-06-05 12:49:10 +0800

    将状态机集成到世界运行时中

--- src/host_engine/core/world.rs
+++ src/host_engine/core/world.rs
@@ -1,9 +1,10 @@
 // 引入结构体
-use super::EngineClock;
+use super::{EngineClock, RuntimeSession};
 
 // 运行时世界结构体
 pub struct RuntimeWorld {
   pub clock: EngineClock,
+  pub session: RuntimeSession,
 }
 
 // 运行时世界实现块
@@ -11,6 +12,7 @@ impl RuntimeWorld {
   pub fn new() -> Self {
     Self {
       clock: EngineClock::new(),
+      session: RuntimeSession::new(),
     }
   }
 }
